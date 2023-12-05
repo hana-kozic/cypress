3ec862602ecdfbf934cbb3b9deac8d604b09b7f8
hana-kozic <75779216+hana-kozic@users.noreply.github.com>
2023-12-05 14:35:26 +0100

    second test

---
+++ cypress/e2e/webdriver-uni/test-file.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe("Handling IFrame & Modals", ()=>{
+    it("Handle webdriveruni iframe and modal", ()=>{
+       
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#iframe').invoke('removeAttr', 'target').click({force: true})
+       
+    });
+
+ 
+})
+
+// novi commit, test

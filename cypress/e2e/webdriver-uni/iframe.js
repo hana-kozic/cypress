ba619813e0e6ec320c7337d6bc5e3348e7815a7a
hana-kozic <75779216+hana-kozic@users.noreply.github.com>
2023-12-10 17:32:34 +0100

    checkboxes

--- cypress/e2e/webdriver-uni/iframe.js
+++ cypress/e2e/webdriver-uni/iframe.js
@@ -1,13 +1,30 @@
-/// <reference types="Cypress" />
+/// <reference types="cypress" />
 
-describe("Handling IFrame & Modals", ()=>{
-    it("Handle webdriveruni iframe and modal", ()=>{
-       
+describe("Handling IFrame & Modals", () => {
+    it("Handle webdriveruni iframe and modal", () => {
+
         cy.visit("http://www.webdriveruniversity.com")
-        cy.get('#iframe').invoke('removeAttr', 'target').click({force: true})
-       
+        cy.get('#iframe').invoke('removeAttr', 'target').click({ force: true })
+
+
+        cy.get('#frame').then($iframe => {
+            const body = $iframe.contents().find('body')
+            cy.wrap(body).as('iframe')
+        })
+        cy.get('@iframe').find('#button-find-out-more').click()
+        //we can use cypress comands, such as .click()
+        //because we used wrap command above
+
+        cy.get('@iframe').find('#myModal').as('modal')
+
+        cy.get('@modal').should(($expectedText)=>{
+            const text=$expectedText.text()
+            expect(text).to.include('Welcome to webdriveruniversity.com we sell a wide range of electrical goods');
+        })
+
+        cy.get('@modal').contains('Close').click()
     });
 
- 
+
 })
 

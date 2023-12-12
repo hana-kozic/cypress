1e0689d01f68170a38fcd5e7c23a0c470d9667d3
hana-kozic <75779216+hana-kozic@users.noreply.github.com>
2023-12-12 13:56:22 +0100

    mouse actions (drag and drop, double click and hold)

---
+++ cypress/e2e/webdriver-uni/mouse-actions.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe("Test mouse actions", () => {
+    it("Scroll element into view", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#actions').scrollIntoView().invoke('removeAttr', 'target').click({ force: true })
+    });
+
+    it("I should be able to drag and drop a draggable item", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#actions').scrollIntoView().invoke('removeAttr', 'target').click({ force: true })
+
+        cy.get('#draggable').trigger('mousedown', { which: 1 }) //which: 1 is oging to click on the center of that given element
+        cy.get('#droppable').trigger('mousemove').trigger('mouseup', { force: true }) //force:true to ensure that it does release the mouse
+    }); 
+
+    it("I should be able to perform a double mouse click", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#actions').scrollIntoView().invoke('removeAttr', 'target').click({ force: true })
+
+       cy.get('#double-click').dblclick();
+    }); 
+
+    it.only("I should be able to hold down the left mouse click button on a given element", () => {
+        cy.visit("http://www.webdriveruniversity.com")
+        cy.get('#actions').scrollIntoView().invoke('removeAttr', 'target').click({ force: true })
+
+       cy.get('#click-box').trigger('mousedown', {which: 1}).then(($element)=>{
+        expect($element).to.have.css('background-color', 'rgb(0, 255, 0)')
+       })
+    }); 
+})

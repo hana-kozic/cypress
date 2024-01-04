db2cf31ecf400700832b8c37e9104967acd74cc1
hana-kozic <75779216+hana-kozic@users.noreply.github.com>
2024-01-04 12:04:24 +0100

    custom commands

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -1,26 +1,27 @@
 /// <reference types="cypress" />
 
 describe("Iterate over elements", () => {
-    it("Log information of all hair care products", () => {
+    beforeEach(function () {
         cy.visit("https://automationteststore.com/")
         cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
+    })
 
+    it("Log information of all hair care products", () => {
         cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
             // $el is a wrapped jQuery element
-            cy.log("Index: "+ index + ": "+$el.text())
+            cy.log("Index: " + index + ": " + $el.text())
         })
-
     });
-    
+
     it("Add specific product to basket", () => {
-        cy.visit("https://automationteststore.com/")
-        cy.get("a[href*='product/category&path=']").contains("Hair Care").click()
-    
-    
-        cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
-            if($el.text().includes('Curls to straight Shampoo')){
-                cy.wrap($el).click()
-            }
-        })
+        cy.selectProduct('Curls to straight Shampoo');
+    });
+
+    it("Add another specific product to basket", () => {
+        cy.selectProduct('Seaweed Conditioner');
+    });
+
+    it("Add another specific product to basket2", () => {
+        cy.selectProduct('Eau Parfumee au The Vert Shampoo');
     });
 })

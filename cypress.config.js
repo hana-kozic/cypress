37f0575fb0b53618760aaad978cb46843f57959b
hana-kozic <75779216+hana-kozic@users.noreply.github.com>
2024-02-20 17:29:03 +0100

    reports

--- cypress.config.js
+++ cypress.config.js
@@ -20,6 +20,10 @@ module.exports = defineConfig({
     videoUploadOnPasses: true,
     viewportHeight: 1080,
     viewportWidth: 1920,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+    configFile: 'reporter-config.json',
+    },
     env: {
       first_name: "Sarah",
       webdriveruni_homepage: "http://www.webdriveruniversity.com"
